b27adec9680edd01fb4950123f7e58bb70a878c3
oRioL-04 <ferlop26112004@gmail.com>
2025-02-28 22:11:11 +0100

    Implementación Paleta de Colores

--- app/src/main/java/com/example/app/ui/theme/Theme.kt
+++ app/src/main/java/com/example/app/ui/theme/Theme.kt
@@ -9,35 +9,37 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color(0xff46141d), // Bright Blue
+    background = Color(0xFFFFFFFF), // Light Gray
+    surface = Color(0xFFFFFFFF), // White
+    onPrimary = Color(0xFFFFFFFF), // White
+    onSecondary = Color(0xFF000000), // Black
+    onTertiary = Color(0xFF000000), // Black
+    onBackground = Color(0xFF000000), // Black
+    onSurface = Color(0xFF000000) // Black
+)
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val DarkColorScheme = darkColorScheme(
+    primary = Color(0xFFFFFFFF), // Light Purple
+    background = Color(0xff46141d), // Dark Gray
+    surface = Color(0xFF1E1E1E), // Slightly lighter Dark Gray
+    onPrimary = Color(0xff46141d), // Black
+    onSecondary = Color(0xFF000000), // Black
+    onTertiary = Color(0xFF000000), // Black
+    onBackground = Color(0xFFFFFFFF), // White
+    onSurface = Color(0xFFFFFFFF) // White
 )
 
 @Composable
 fun APPTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false, // Disable dynamic color for testing
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
