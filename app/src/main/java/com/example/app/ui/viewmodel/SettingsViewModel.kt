c41aae99899f925799df5d7c378e347fbde0d772
oriol <ferlop26112004@gmail.com>
2025-03-19 10:41:22 +0100

    Language Screen SettingsScreen

--- app/src/main/java/com/example/app/ui/viewmodel/SettingsViewModel.kt
+++ app/src/main/java/com/example/app/ui/viewmodel/SettingsViewModel.kt
@@ -1,20 +1,32 @@
 package com.example.app.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import com.example.app.utils.LanguageManager
+import com.example.app.data.SharedPrefsManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
-    private val languageManager: LanguageManager
+    private val sharedPrefsManager: SharedPrefsManager
 ) : ViewModel() {
 
-    fun changeLanguage(langCode: String) {
-        languageManager.setLocale(langCode)
+    // Variables de estado inicializadas con los valores guardados.
+    var language by mutableStateOf(sharedPrefsManager.userLanguage ?: "es")
+        private set
+
+    var isDarkTheme by mutableStateOf(sharedPrefsManager.darkTheme)
+        private set
+
+    fun updateLanguage(newLanguage: String) {
+        sharedPrefsManager.userLanguage = newLanguage
+        language = newLanguage
     }
 
-    fun getCurrentLanguage(): String {
-        return languageManager.getSavedLanguage()
+    fun updateDarkTheme(isDark: Boolean) {
+        sharedPrefsManager.darkTheme = isDark
+        isDarkTheme = isDark
     }
 }
